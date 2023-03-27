8adb53641c83d656fc035eb7055613d86fcbad59
miren-doeun <111549068+miren-doeun@users.noreply.github.com>
2023-03-27 10:46:31 +0900

    03.27

---
+++ 0327/03.27.c
@@ -0,0 +1,29 @@
+/*
+ * 03.27.c
+ *
+ * Created: 2023-03-27 ¿ÀÀü 8:53:35
+ * Author: user
+ */
+
+#include <mega128.h>
+#include <delay.h>
+
+void main(void)
+{
+while (1)
+    {
+    // Please write your application code here
+      int i;
+      unsigned char led;
+      DDRC = 0xFF;
+      
+      while(1){
+          led = 0b01111111;
+          for(i=0;i<8;i++){
+          PORTC=led;
+          delay_ms(500);
+          led>>=1;
+          }
+      }
+    }
+}
---
+++ 0327/03.27_3.c
@@ -0,0 +1,19 @@
+#include <mega128.h>
+#include <delay.h>
+
+void main(void)
+{
+    int i;
+    unsigned char led;
+    DDRC=0xFF;
+    while (1)
+          {
+           led = 0xFE;
+           for(i=0;i<8;i++){
+                PORTC=led;
+                delay_ms(500);
+                led<<=1;
+                led = led|0b00000001;
+           }
+          }
+}
